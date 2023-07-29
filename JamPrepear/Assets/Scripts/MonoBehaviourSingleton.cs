3b73f18e49c9448c7938c7ffbb9131d98363eb26
Ihor Chuchman <kodlon.igorchuchman@gmail.com>
2023-07-29 14:25:09 +0300

    Added mono and classic singleton

---
+++ JamPrepear/Assets/Scripts/MonoBehaviourSingleton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null) 
+                return _instance;
+            
+            var singletonObject = new GameObject();
+            _instance = singletonObject.AddComponent<T>();
+            singletonObject.name = typeof(T) + " Singleton";
+
+            DontDestroyOnLoad(singletonObject);
+
+            return _instance;
+        }
+    }
+}
