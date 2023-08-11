f702c7c93fac9ab48ffb69dbc1c1ab23bdd382c1
Ihor Chuchman <kodlon.igorchuchman@gmail.com>
2023-08-12 02:56:50 +0300

    Tested more navmesh and added camera shake script

---
+++ JamPrepear/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [SerializeField] private float _shakeAmount = 0.1f;
+
+    private Vector3 _originalLocalPosition;
+    private float _currentShakeDuration;
+
+    private void Start()
+    {
+        _originalLocalPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        if (_currentShakeDuration > 0)
+        {
+            transform.localPosition = _originalLocalPosition + Random.insideUnitSphere * _shakeAmount;
+            _currentShakeDuration -= Time.deltaTime;
+        }
+        else
+        {
+            _currentShakeDuration = 0f;
+            transform.localPosition = _originalLocalPosition;
+        }
+    }
+
+    public void ShakeCamera(float customDuration, float customAmount)
+    {
+        _currentShakeDuration = customDuration;
+        _shakeAmount = customAmount;
+    }
+}
